a9438a40fcbd53e3a3f9338551201bdb854fcf27
youngjune kim <java99i@gmail.com>
2025-01-24 08:42:28 +0900

    python3문법으로 변경

--- Generators/square_nums.py
+++ Generators/square_nums.py
@@ -1,4 +1,3 @@
-
 # def square_numbers(nums):
 #     for i in nums:
 #         yield (i*i)
@@ -7,7 +6,7 @@
 
 my_nums = (x*x for x in [1,2,3,4,5])
 
-print list(my_nums) # [1, 4, 9, 16, 25]
+print(list(my_nums))  # [1, 4, 9, 16, 25]
 
 # for num in my_nums:
-#     print num
+#     print(num)
